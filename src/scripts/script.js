f97d419f88015820dff05382d17525b15bae489a
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2025-07-29 21:27:59 +0200

    finished let and const practice

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+//Task-1
+
+let admin;
+let name;
+
+name = "John";
+admin = name;
+
+alert(admin);
+
+
+//Task-2
+
+const ourPlanetName = 'Earth';
+const currentUser = "Nik";
+
+//Task-3
+
+const numberType = 10 + '10';
+console.log(typeof numberType); //string
+
