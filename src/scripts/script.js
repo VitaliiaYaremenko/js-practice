da92dc9732b8a6322a0dcee2546dc0e432025b62
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2025-09-18 21:50:36 +0200

    added changes tasks with numbers

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// Task - 1
+
+const firstInput = prompt(`Write please the first number:`, '' );
+const secondInput = prompt(`Write please second number :`, '');
+
+let sum = null;
+
+if(firstInput === null || secondInput === null) {
+    throw new Error('Please put the number');
+}
+
+const firstUserNumber = Number(firstInput);
+const secondUserNumber = Number(secondInput);
+
+
+if(Number.isFinite(firstUserNumber) && Number.isFinite(secondUserNumber)) {
+    sum = firstUserNumber + secondUserNumber;
+
+    alert(`${firstUserNumber} + ${secondUserNumber} = ${sum}`);
+} else {
+    throw new Error('Pun the number please');
+}
+
